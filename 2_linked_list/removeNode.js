06d325d5b0a17ec196a2a65bcbd6cc27df69830d
Philip Browne <pbrowne@gmail.com>
2023-03-09 12:54:25 -0500

    feat: add removeNode

---
+++ 2_linked_list/removeNode.js
@@ -0,0 +1,30 @@
+class Node {
+    constructor(val) {
+      this.val = val;
+      this.next = null;
+    }
+  }
+
+const removeNodeIterative = (head, target) => {
+    if (head === null) return null
+    if (head.val === target) return head.next
+    let current = head
+    let previous = null
+    while (current !== null) {
+        if (current.val === target) {
+            const next = current.next
+            prev.next = next
+            break
+        }
+        prev = current
+        current = current.next
+    }
+    return head
+}
+
+const removeNode = (head, target, prev=null) => {
+    if (head === null) return null
+    if (head.val === target) return head.next
+    head.next = removeNode(head.next, target, head)
+    return head
+}
